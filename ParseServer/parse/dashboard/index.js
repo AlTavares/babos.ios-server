d5344139a9ace9b1d017af6e9b46cedfcdff17a8
AlTavares <alexandre@live.in>
2016-08-21 19:53:45 -0300

    Server files

---
+++ ParseServer/parse/dashboard/index.js
@@ -0,0 +1,24 @@
+var ParseDashboard = require('parse-dashboard');
+
+let serverURL = "http://localhost:3000/parse"
+if (process.env.NODE_ENV == 'production') {
+  serverURL = 'https://altavares.tk/parse'
+}
+var dashboard = new ParseDashboard({
+  "apps": [
+    {
+      "serverURL": serverURL,
+      "appId": process.env.appId,
+      "masterKey": process.env.masterKey,
+      "appName": "MyApp"
+    }
+  ],
+  "users": [
+    {
+      "user": process.env.username,
+      "pass": process.env.password
+    }
+  ]
+});
+
+module.exports = dashboard
