ac3a59c3310eb010aed976d14501d362f3e99645
AlTavares <alexandre@live.in>
2016-10-09 20:32:55 -0300

    Testing localhost on heroku

--- parse/dashboard/index.js
+++ parse/dashboard/index.js
@@ -1,10 +1,11 @@
 'use strict'
 var ParseDashboard = require('parse-dashboard');
+var port = process.env.PORT || 3000
 
-let serverURL = "http://localhost:3000/parse"
-if (process.env.NODE_ENV == 'production') {
-  serverURL = 'https://babosio.herokuapp.com/parse'
-}
+let serverURL = 'http://localhost:' + port + '/parse'
+// if (process.env.NODE_ENV == 'production') {
+//   serverURL = 'https://babosio.herokuapp.com/parse'
+// }
 var dashboard = new ParseDashboard({
   "apps": [
     {
